86fab4d846771450ab612c63d90ea432e81da6be
LAD021 <1269658899@qq.com>
2020-12-15 22:47:49 +0800

    time duration solved

--- graphicgo/refresher.go
+++ graphicgo/refresher.go
@@ -45,7 +45,7 @@ func (job *refreshJob) Start() {
 		for {
 			select {
 			case <-ticker.C:
-				refreshBgColor()
+				refreshBgColor() // test
 				resetScreen()
 			case cmd := <-job.chCmd:
 				if cmd == StopCmd {

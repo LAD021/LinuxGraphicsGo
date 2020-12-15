4d29ede72767a038ce9660739edc1ce1cb8a9af9
LAD021 <1269658899@qq.com>
2020-12-15 16:23:31 +0800

    retry

--- graphicgo/config.go
+++ graphicgo/config.go
@@ -2,6 +2,6 @@ package graphicgo
 
 const devPath string = "/dev/fb0"
 
-const screenHeight = 768
+const screenHeight = 777777768
 const screenWidth = 1280
-const pixWidth	  = 4
+const pixWidth	  = 4 
